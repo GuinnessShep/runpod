74f6ff8b2daedad15399c870b0a89d7aeda5ca9a
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-11-12 00:07:53 -0800

    Update textgen_setup.sh

--- textgen_setup.sh
+++ textgen_setup.sh
@@ -2,5 +2,6 @@
 conda activate textgen
 python3 -m pip install --upgrade pip
 pip install -r /requirements.txt
+pip install autoawq
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 conda install -y -c "nvidia/label/cuda-12.1.0" cuda-runtime
